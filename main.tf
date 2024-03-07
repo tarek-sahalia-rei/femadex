b82382de5385a204b56063e9e570634339a032ce
tarek-sahalia-rei <tarek.sahalia@reisystems.com>
2024-03-07 13:27:48 -0500

    destroy

--- main.tf
+++ main.tf
@@ -3,28 +3,28 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-FEMADEx-test"
+  name     = "rg-FEMADEx"
   location = "East US"
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "femadex"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
+# resource "azurerm_virtual_network" "vnet" {
+#   name                = "femadex"
+#   address_space       = ["10.0.0.0/16"]
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+# }
 
-resource "azurerm_subnet" "subnet1" {
-  name                 = "privateSubnet1"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
+# resource "azurerm_subnet" "subnet1" {
+#   name                 = "privateSubnet1"
+#   resource_group_name  = azurerm_resource_group.rg.name
+#   virtual_network_name = azurerm_virtual_network.vnet.name
+#   address_prefixes     = ["10.0.1.0/24"]
+# }
 
 
-resource "azurerm_subnet" "public_subnet" {
-  name                 = "publicSubnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
+# resource "azurerm_subnet" "public_subnet" {
+#   name                 = "publicSubnet"
+#   resource_group_name  = azurerm_resource_group.rg.name
+#   virtual_network_name = azurerm_virtual_network.vnet.name
+#   address_prefixes     = ["10.0.2.0/24"]
+# }
